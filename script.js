53f09ad1db9b865f57665484a90e64e9f4308d7c
Natacha <natacha.har@onja.org>
2020-07-18 11:58:45 +0300

    Created modal

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('good luck!');
+const button = document.querySelector('.add-order');
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+
+const openModal = event => {
+    outerModal.classList.add('open');
+};
+
+const closeModal = event => {
+    const isOutside = !event.target.closest('inner-modal');
+    if(isOutside) {
+        outerModal.classList.remove('open');
+    }
+};
+
+const escapeModal = event => {
+    if (event.key === 'Escape') {
+        outerModal.classList.remove('open');    
+    }
+};
+
+window.addEventListener('keydown', escapeModal);
+outerModal.addEventListener('click', closeModal);
+button.addEventListener('click', openModal);
