a1486ee7b24491fd070a5ad84667b2f6097735a5
Natacha <natacha.har@onja.org>
2020-07-20 10:43:13 +0300

    fixed new html and fixed the detail using event delegation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+//grabbing elements
 const button = document.querySelector('.add-order');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
-
 const orderList = document.querySelector('.order-list')
 const order = document.querySelector('.order');
 const title = document.querySelector('#name');
@@ -11,53 +11,96 @@ const btn = document.querySelector('.submitOrder');
 const select = document.querySelector('.select-form');
 const form = document.querySelector('form');
 
+//  
 const openModal = event => {
     outerModal.classList.add('open');
+    innerModal.innerHTML = `
+    <form>
+		<p>Your name :</p>
+		<input class="input-form" type="text" id="name" name="name" placeholder="Enter your name here"
+			required />
+		<p>Please select your dish :</p>
+		<select name="dish" class="select-form" required>
+			<option value="romazava">Romazava</option>
+			<option value="koba">Koba</option>
+			<option value="ravitoto">Ravitoto</option>
+			<option value="mokary">Mokary</option>
+			<option value="achard">Achard</option>
+			<option value="masikita">Masikita</option>
+			<option value="sambos">Sambos</option>
+			<option value="mofo-baolina">Mofo baolina</option>
+			<option value="ranonapango">Ranonapango</option>
+		</select>
+		<p>Please select the size of your plate :</p>
+			<input type="radio" id="small" name="size" value="small" required />
+			<label for="small">Small</label><br />
+			<input type="radio" id="medium" name="size" value="medium" />
+			<label for="medium">Medium</label><br />
+			<input type="radio" id="large" name="size" value="large" />
+			<label for="large">Large</label><br />
+			<p>How many pieces do you want to order?</p>
+			<input class="input-form" type="number" id="quantity" name="amount"
+				placeholder="Enter a number here" required />
+			<button class="submitOrder" type="submit">Add this order</button>
+	</form>
+    `; 
 };
 
+// 
 const closeModal = event => {
-    const isOutside = !event.target.closest('inner-modal');
-    if(isOutside) {
+    const isOutside = !event.target.closest('.inner-modal');
+    if (isOutside) {
         outerModal.classList.remove('open');
     }
 };
 
-  const escapeModal = event => {
-     if (event.key === 'Escape') {
-        outerModal.classList.remove('open');    
-     }
-  };
+const escapeModal = event => {
+    if (event.key === 'Escape') {
+        outerModal.classList.remove('open');
+    }
+};
 
 window.addEventListener('keydown', escapeModal);
-// outerModal.addEventListener('click', closeModal);
+outerModal.addEventListener('click', closeModal);
 button.addEventListener('click', openModal);
 
-btn.addEventListener('click', ($event) => {
-    $event.preventDefault();
-    // outerModal.classList.remove('outer-modal');
-    outerModal.style.display = 'none';
-    const myNewHtml = `
-        <div class="new-order">
-            <span class="title">
-                ${title.value}
-            </span>
-            <button class="details">Details</button>
-            <button class="served">Delete order</button>
-        </div>
-    `;
-    orderList.insertAdjacentHTML('afterbegin', myNewHtml);
-});
+const theOrder = () => {
+     const myNewHtml = `
+         <div class="new-order">
+             <span class="title">
+             </span>
+             <button class="details">Details</button>
+             <button class="served">Delete order</button>
+         </div>
+     `;
+     orderList.insertAdjacentHTML('afterbegin', myNewHtml);
+     outerModal.classList.remove('open');
+ };
+
+
 
 detail.addEventListener('click', ($event) => {
     $event.preventDefault();
     const myHtml = `
-        <div class="order-detail">
-            <h3>${title.value}</h3>
-            <h5>Order<br>
-        </div>
-    `;  
-    innerModal.innerHTML = myHtml; 
+         <div class="order-detail">
+             <h3></h3>
+             <h5>Order<br></h5>
+         </div>
+     `;
+    innerModal.innerHTML = myHtml;
 });
 
+// Event delegation
+window.addEventListener ('click', ($event) => {
+    if ($event.target.matches('button.submitOrder')) {
 
+    };
+    // Show detail
+    if (event.target.matches('button.details')) {
+        outerModal.classList.add('open');
+    };
 
+    if(event.target.matches('button.served')) {
+        outerModal.classList.remove('.order');
+    }
+});
